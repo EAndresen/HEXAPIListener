a82b84c5b00f91924fcf2fe0af519984ce55e6ac
Eric Andresen <eric@andresen.codes>
2017-10-04 20:15:17 +0200

    * Added a loop to the thread to run the program for ever and listen to socket input.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,21 +7,25 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Runnable serverTask = () -> {
-            try {
-                ServerSocket serverSocket = new ServerSocket(5000);
-                Socket socket = serverSocket.accept();
-                InputStream stream = socket.getInputStream();
-                Scanner reader = new Scanner(stream);
+    public static void main(String[] args) throws IOException {
 
-                    while (reader.hasNextLine()) {
-                        System.out.println(reader.nextLine());
-                    }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        };
+        ServerSocket serverSocket = new ServerSocket(5000);
+
+        while (true) {
+            Runnable serverTask = () -> {
+                try {
+                    Socket socket = serverSocket.accept();
+                    InputStream stream = socket.getInputStream();
+                    Scanner reader = new Scanner(stream);
+
+                        while (reader.hasNextLine()) {
+                            System.out.println(reader.nextLine());
+                        }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            };
         serverTask.run();
+        }
     }
 }
